37c61e6076b196dea347fd850e20beaad460a782
eletroswing <ytalosilvabatalha@gmail.com>
2024-05-04 11:23:52 -0300

    feat(resource): adding transfer resource and tests for it

---
+++ src/clients/transfer/create/types.ts
@@ -0,0 +1,10 @@
+import { Transfer } from "../commonTypes";
+
+export  type CreatePayload = {
+  value: number;
+  fromPixKey: string;
+  toPixKey: string;
+}
+export  type CreateResponse = {
+  transaction: Transfer;
+};
