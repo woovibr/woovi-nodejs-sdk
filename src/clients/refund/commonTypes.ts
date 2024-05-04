a0bb2a3507f8481cccfe5e1f27a6dd78201f1179
eletroswing <ytalosilvabatalha@gmail.com>
2024-05-04 00:12:51 -0300

    feat(refund): add resource for refund and tests for it

---
+++ src/clients/refund/commonTypes.ts
@@ -0,0 +1,8 @@
+export interface Refund {
+    value: number;
+    status: "IN_PROCESSING" | "REFUNDED" | "NOT_ACCOMPLISHED";
+    correlationID: string;
+    refundId: string;
+    time: string;
+    comment: string;
+  }
