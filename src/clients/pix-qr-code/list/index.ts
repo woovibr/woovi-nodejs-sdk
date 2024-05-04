0a8fa4d105b51a3874fcdfe4fc6715047010a0af
eletroswing <ytalosilvabatalha@gmail.com>
2024-05-04 11:45:28 -0300

    fix(resources): fix pagination

--- src/clients/pix-qr-code/list/index.ts
+++ src/clients/pix-qr-code/list/index.ts
@@ -1,6 +1,8 @@
 import { RestClientApi } from "@utils/types";
 import { ListResponse } from "./types";
+import { Pagination } from "@src/types";
+import { objectToQueryString } from "@utils/restClient";
 
 export default (restClient: RestClientApi) => {
-    return () => restClient<ListResponse>('/api/v1/qrcode-static');
+    return (pagination: Pagination = {limit: 10, skip: 0}) => restClient<ListResponse>(`/api/v1/qrcode-static?${objectToQueryString({...pagination})}`);
 }
