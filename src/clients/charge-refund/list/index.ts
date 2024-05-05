ae4c8aa84d443f0b5009f6b91d7555a7244a0396
criskell <cris.moraes.ds@gmail.com>
2024-05-04 23:30:55 -0300

    feat(charge-refund): add resource

---
+++ src/clients/charge-refund/list/index.ts
@@ -0,0 +1,13 @@
+import { RestClientApi } from "@utils/types";
+import { GetPayload, ListResponse } from "./types";
+import { Pagination } from "@src/types";
+import { objectToQueryString } from "@utils/restClient";
+
+export default (restClient: RestClientApi) => {
+  return (data: GetPayload, pagination: Pagination = { limit: 10, skip: 0 }) =>
+    restClient<ListResponse>(
+      `/api/v1/charge/${data.chargeId}?${objectToQueryString({
+        ...pagination,
+      })}`
+    );
+};
